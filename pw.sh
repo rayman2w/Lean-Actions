1c7a311ea4f3a9e8ce5195bc82b77113ac81f3ad
“kiss2u” <joe.lamplight@gmail.com>
2019-12-11 08:27:09 +0000

    lede + passwall

--- pw.sh
+++ pw.sh
@@ -7,3 +7,5 @@
 #=================================================
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+sed -i '3a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf$
+./scripts/feeds update -a && ./scripts/feeds install -a
