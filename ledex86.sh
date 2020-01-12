99a362e1503ef890273215c652fb7b3927197d3d
kiss2u <joe.lamplight@gmail.com>
2020-01-12 03:53:49 +0000

    leanop x64 vssr and gz

--- ledex86.sh
+++ ledex86.sh
@@ -7,5 +7,6 @@
 #=================================================
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+sed -i 's/bootstrap/argon/g' openwrt/package/feeds/luci/luci-base/root/etc/config/luci
 sed -i '3a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 ./scripts/feeds update -a && ./scripts/feeds install -a
