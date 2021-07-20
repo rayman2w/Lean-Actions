9e53d9b43dc0fd9f1c430b6e5b54dbbadff27132
kiss2u <joe.lamplight@gmail.com>
2021-07-20 20:36:03 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -9,4 +9,4 @@
 #============================================================
 
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.100.2/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.100.3/g' package/base-files/files/bin/config_generate
